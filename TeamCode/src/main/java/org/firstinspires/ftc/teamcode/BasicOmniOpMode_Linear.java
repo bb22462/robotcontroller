a64c84291b59ffcfd3479c2f17a76030dd0b5000
Index:Zero <12856517-indx0@users.noreply.gitlab.com>
2022-11-20 08:50:13 +0000

    Hand control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -43,6 +45,10 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    private DcMotor upDrive = null;
+    private Servo handServo = null;
+    private final double MAX_POS = 1.0;
+    private final double MIN_POS = 0.0;
 
     @Override
     public void runOpMode() {
@@ -52,7 +58,9 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive"); // Motor 3
         leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive"); // Motor 0
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive"); // Motor 2
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive"); // Motor 1
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        upDrive = hardwareMap.get(DcMotor.class, "up_drive"); // Drive to move robot's hand up and down
+        handServo = hardwareMap.get(Servo.class, "hand_servo"); // Servo in robot's hand
 
         // ########################################################################################
         // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
@@ -68,6 +76,8 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        upDrive.setDirection(DcMotor.Direction.REVERSE);
+        upDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -78,7 +88,11 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            // TODO Make hand control with sticks
             double max;
+
+            double handServoPos = handServo.getPosition();
+            int upDrivePos = upDrive.getCurrentPosition();
 
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
@@ -105,6 +119,36 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
                 rightBackPower  /= max;
             }
 
+            if (gamepad2.b) {
+                if (handServoPos == MAX_POS) {
+                    System.out.println("Hand is already opened.");
+                }
+                else {
+                    handServo.setPosition(1.0);
+                }
+            }
+
+            if (gamepad2.x) {
+                if (handServoPos == MIN_POS) {
+                    System.out.println("Hand is already closed.");
+                }
+                else {
+                    handServo.setPosition(0.0);
+                }
+            }
+
+            if (gamepad2.y) {
+                upDrive.setPower(0.5);
+            }
+
+            else if (gamepad2.a) {
+                upDrive.setPower(-0.3);
+            }
+            else {
+                upDrive.setPower(0);
+            }
+
+
             // This is test code:
             //
             // Uncomment the following code to test your motor directions.
@@ -130,6 +174,7 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("UpDrive", "UpDrive Position: " + String.valueOf(upDrivePos));
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
